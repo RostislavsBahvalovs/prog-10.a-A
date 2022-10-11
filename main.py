9a1e7c2fd1e4eb9300e0bfac2437e67a1400aabd
rosstik <115002366+RostislavsBahvalovs@users.noreply.github.com>
2022-10-11 12:20:53 +0000

    2.uzd

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-sk1=int(input("Ievadiet skaitli: "))
-if sk1>10:
-  print("Skaitlis ir liels")
+from math import *
+sk1=int(input ("Ievadiet pirmo skaitli: "))
+sk2=int(input ("Ievadiet otro skaitli: "))
+oper=input("Ievadiet matemātisko operāciju: ")
+if oper=="+":
+  print("Summa = ", sk1+sk2)
+elif oper=="-":
+  print("Atņemšanas rezultāts =", sk1-sk2)
+elif oper=="*":
+  print("Reizinäjums ", sk1*sk2)
+elif oper=="/":
+  print("Dalijums =", sk1/sk2)
+elif oper=="s":
+  print("Kvadrātsakne =", sqrt(sk1))
 else:
-  print("Skaitlis ir mazs")
+  print("Sāda matemātiskä operācija netiek atbalstita!!!")
